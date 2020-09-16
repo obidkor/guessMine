ad591f4489f32384de9217b31a4b67d8ac6ed1f9
obidkor <coavldjs93@naver.com>
2020-09-17 00:10:17 +0900

    socketio setup compl

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
+  //config
   env: {
     browser: true,
     es6: true,
     node: true,
   },
+  // 직접 vscode extension 깔아야댐(eslint,prettier)
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
   },
+
+  //무시할 에러들
   rules: {
+    // console log omit
     "no-console": "off",
+
+    // 줄바꿈 에러
     "prettier/prettier": [
       "error",
       {
--- src/server.js
+++ src/server.js
@@ -1,16 +1,27 @@
-import { join } from "path";
-import express from "express";
-import socketIO from "socket.io";
+import { join } from "path"; // dir 모듈
+import express from "express"; // http 서버 모듈
+import socketIO from "socket.io"; // socket 통신 모듈
 
 const PORT = 5000;
+//서버 variable
 const app = express();
+
+// config
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+
+// static
 app.use(express.static(join(__dirname, "static")));
+
+// view resolve
 app.get("/", (req, res) => res.render("home"));
+
+// launch message
 const handleListiening = () =>
   console.log(`✔🤣 Server ruuning : http://localhost:${PORT}`);
 
+// server 구동(listen)
 const server = app.listen(PORT, handleListiening);
 
+// socket variable(아키텍처 : express 위에 socketio)
 const io = socketIO(server);
